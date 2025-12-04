d8e3329d6a19b5f3c433f5aae740c0f8c876eabd
kelesbilall <bilal.keeles@gmail.com>
2025-12-04 19:42:42 +0300

    bilal push 1.0
    hash util init

--- src/util/HashUtil.java
+++ src/util/HashUtil.java
@@ -1,5 +1,36 @@
 package util;
 
+import java.security.MessageDigest;
+
 public class HashUtil {
+
+    public static String hash(String password){
+
+        try {
+
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[]  encodedhash = md.digest(password.getBytes());
+        
+            StringBuilder sb = new StringBuilder();
+        
+            for(byte b : encodedhash){
+
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+        } 
+
+        catch (Exception e){
+            throw new RuntimeException("An error occurred while hashing the password!",e);
+        }
+        /* girilen plain text şifre hashlernir ve storedHash ile karşılaştırır,
+        eğer eşlenirse true döner */
+    }
+
     
+    public static boolean verify(String plainPassword, String storedHashString){
+        
+        return hash(plainPassword).equalsIgnoreCase(storedHashString);
+    }
+
 }
