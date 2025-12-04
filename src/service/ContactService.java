26193b48b0c58726d23d986ebe6c29f5bea32976
ayhanoner <ayhannoner12@gmail.com>
2025-12-04 22:22:25 +0300

    ayhan 1.0
    contactservice, testermenu

--- src/service/ContactService.java
+++ src/service/ContactService.java
@@ -1,5 +1,55 @@
 package service;
 
+import dao.ContactDAO;
+import model.Contact;
+import java.util.List;
+
+import javax.management.Query;
+
 public class ContactService {
+    private final ContactDAO contactDAO;
+
+    public ContactService() {
+        this.contactDAO = new ContactDAO();
+    }
+
+    public List<Contact> getAllContacts() {
+        return contactDAO.getAllContacts();
+    }
+
+    public List<Contact> getAllSorted(String sortField, boolean ascending) {
+        return contactDAO.getAllSorted(sortField, ascending);
+    }
+
+    public List<Contact> searchByFirstName(String query) {
+        return contactDAO.searchByFirstName(query);
+    }
     
+    public List<Contact> searchByLastName(String query) {
+        return contactDAO.searchByLastName(query);
+    }
+    public List<Contact> searchByPhoneContains(String digits) {
+        return contactDAO.searchByPhoneContains(digits);
+    }
+
+    public void printContactsList(List<Contact> contacts) {
+        if (contacts == null || contacts.isEmpty()) {
+            System.out.println("No contacts found.");
+            return;
+        }
+        System.out.println("\n CONTACTS LIST");
+        System.out.printf("\n %-5s %-15s %-15s %-15s %-25s %-30s", "ID", "FIRST NAME", "LAST NAME", "PHONE", "EMAIL", "ADDRESS");
+        System.out.println("\n-----------------------------------------------------------------------------------------------");
+        
+        for (Contact contact : contacts) {
+            String firstName = contact.getFirstName() != null ? contact.getFirstName() : "-";
+            String lastName = contact.getLastName() != null ? contact.getLastName() : "-";
+            String phone = contact.getPhoneNumber() != null ? contact.getPhoneNumber() : "-";
+            String email = contact.getEmail() != null ? contact.getEmail() : "-";
+
+            System.out.printf(" %-5d %-15s %-15s %-15s %-25s %-30s",contact.getContactId(), firstName, lastName, phone, email);
+
+            System.out.println("\n Total " + contacts.size() + " contact(s) found.");
+        }
+    }
 }
